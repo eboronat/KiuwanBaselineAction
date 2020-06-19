1e6073d9c673a09f9098a88569853a08079b71f8
eboronat <43843747+eboronat@users.noreply.github.com>
2020-06-19 15:04:28 +0200

    Update kla.py

--- kla.py
+++ kla.py
@@ -75,7 +75,8 @@ def getBLAnalysisResultsURL(a_c, kla_user=PARAM_KLA_USERNAME, kla_password=PARAM
     apicall = apicall + "/apps/analysis/" + a_c
     print('Calling REST API [', apicall, '] ...')
 
-    authString = base64.b64encode(kla_user + ':' + kla_password)
+    kla_password = kla_password.decode('string_escape')
+    authString = base64.encodebytes(('%s:%s' % (kla_user,kla_password)).encode()).decode().strip()
     
     if "domain-id" in advanced:
       posDomain = advanced.find("domain-id")
